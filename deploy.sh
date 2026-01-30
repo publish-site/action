b65dcadae3bfaa1c32f0f62a2d6d6b50ea562322
a-rvid <a-rvid@rvid.eu>
2026-01-30 16:33:52 +0100

    Site

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 printf "Dry run: "
+
 if [[ $DRY_RUN == true ]]; then 
   echo enabled.
+  agent="DRY"
 else
   echo disabled.
+  agent="Actions"
 fi
 
 if [[ -d "${TARGET}" ]]; then
-  tar -czvf "${TARGET}"
-else if
+  tar -czvf upload.tar.gz "${TARGET}"
+#else if
   
 fi
 
-curl -X POST "${URL}"
+curl --data-binary @upload.tar.gz -X POST -A "$agent" "${URL}"
