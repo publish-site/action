b00951b40e0b25189dc736f621b0d24c020d61b9
a-rvid <a-rvid@rvid.eu>
2026-02-09 19:11:52 +0100

    PKI stuff

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+TMP=$(mktemp -d)
 printf "Dry run: "
 
 if [[ $DRY_RUN == true ]]; then 
@@ -10,12 +11,25 @@ else
   agent="Actions"
 fi
 
-if [[ -d "${TARGET}" ]]; then
-  tar -czvf upload.tar.gz -C "${TARGET}" .
-#else if
-  
+if [[ -n "${TARGET}" ]]; then
+  tar -czvf "$TMP/upload.tar.gz" -C "${TARGET}" .
+else
+  echo "Target directory not set. Exiting"
+  exit 1
 fi
 
-base64 upload.tar.gz > encoded
+if [[ -n "$PRIVKEY" ]]; then
+  base64 -d <<< "$PRIVKEY" > "$TMP/client.key"
+else
+  echo "Private key (privkey input) not set. Exiting"
+  exit 1
+fi
 
-curl -d @encoded -XPOST -A "$agent" "${URL}" -vv
+if [[ -n "$CERT" ]]; then
+  base64 -d <<< "$PRIVKEY" > "$TMP/client.key"
+else
+  echo "Certificate (cert input) not set. Exiting"
+  exit 1
+fi
+
+base64 "$TMP/upload.tar.gz" | curl -d @- -A "$agent" "${URL}" -vv --cert /tmp/client.key
--- site/pki/pki.sh
+++ site/pki/pki.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo Certificates
 fqdn="api.localhost.rvid.eu"
 mail="."
 PWD=$(pwd)
-cd $(mktemp -d)
+TMP=$(mktemp -d)
+cd "$TMP"
 
 helpcmd () {
     echo "Usage: ./pki.sh -s api.localhost.rvid.eu"
@@ -18,7 +18,7 @@ while getopts ":n:v" opt; do
   esac
 done
 
-echo "Generating CA certificates."
+echo -e "\033[92mGenerating CA certificates.\033[0m"
 
 mkdir -p /dev/shm/mtls
 chmod 700 /dev/shm/mtls
@@ -28,10 +28,22 @@ openssl req -new -x509 -key /dev/shm/mtls/CA.key -out CA.pem \
     -subj "/CN=${fqdn}/emailAddress=${mail}" \
     -sha256
 
-echo "Generating client certificates (mTLS)"
+echo -e "\033[92mGenerating client certificates (mTLS)\033[0m"
 
 openssl req -newkey rsa:4096 -nodes -keyout client.key -out client.csr -subj "/CN=actions" # Client key and csr
 openssl x509 -req -in client.csr -CA CA.pem -CAkey /dev/shm/mtls/CA.key -out client.pem -sha256 # Client certificate
 
-echo Cleaning up
+echo -e "\033[32mServer Configuration\n\033[92mCLIENT_CA\033[0m:"
+base64 "CA.pem" -w 0
+printf "\n\n\n"
+
+echo -e "\033[32mWorkflow Configuration\n\033[92mprivkey \033[0m(add as action SECRET!):"
+base64 client.key -w 0
+printf "\n\n"
+echo -e "\033[92mcert \033[0m(add as action SECRET):"
+base64 client.pem -w 0
+printf "\n\n\n"
+
 cd "$PWD"
+rm -rf /dev/shm/mtls 
+echo -e "\033[32mCertificates can be directly mounted and are located at \033[0m$TMP"
